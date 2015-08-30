bb2de37373ba94af26280480df72acf46763ab05
galvao <galvao@galvao.eti.br>
2015-08-30 01:42:54 -0300

    Tests were not executing due to namespace issues

--- test/RandomStringTest.php
+++ test/RandomStringTest.php
@@ -1,34 +1,35 @@
 <?php
 require_once '../src/RandomString/RandomString.php';
-class RandomStringTest extends PHPUnit_Framework_TestCase
+
+class RandomStringTest extends \PHPUnit_Framework_TestCase
 {
     public function testLength()
     {
-        $r = new RandomString(12, array('upperCase'));
+        $r = new RandomString\RandomString(12, array('upperCase'));
         $this->assertEquals(strlen($r->result), 12);
     }
 
     public function testUppercaseOnly()
     {
-        $r = new RandomString(12, array('upperCase'));
+        $r = new RandomString\RandomString(12, array('upperCase'));
         $this->assertRegExp('/^[A-Z]+$/', $r->result);
     }
 
     public function testLowercaseOnly()
     {
-        $r = new RandomString(12, array('lowerCase'));
+        $r = new RandomString\RandomString(12, array('lowerCase'));
         $this->assertRegExp('/^[a-z]+$/', $r->result);
     }
 
     public function testLettersOnly()
     {
-        $r = new RandomString(12, array('upperCase', 'lowerCase'));
+        $r = new RandomString\RandomString(12, array('upperCase', 'lowerCase'));
         $this->assertRegExp('/^[A-Z]+$/i', $r->result);
     }
 
     public function testNumbersOnly()
     {
-        $r = new RandomString(12, array('numbers'));
+        $r = new RandomString\RandomString(12, array('numbers'));
         $this->assertRegExp('/^[0-9]+$/i', $r->result);
     }
 }
